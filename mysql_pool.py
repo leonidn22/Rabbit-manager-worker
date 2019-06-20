91e518705d04b9b7a1c0fa02a81bcb2ee9889cc1
leonidn22 <lneizberg@gmail.com>
2019-06-20 12:01:06 +0300

    Update mysql_pool.py

--- mysql_pool.py
+++ mysql_pool.py
@@ -5,8 +5,8 @@ import aiomysql
 import sys
 import traceback
 
-@asyncio.coroutine
-def mysql_insert(loop, stmt_insert):
+# @asyncio.coroutine
+async def mysql_insert(loop, stmt_insert):
     db = {'host': '127.0.0.1',
           'port': 3306,
           'user': 'leo',
@@ -14,10 +14,11 @@ def mysql_insert(loop, stmt_insert):
           'db': 'leo',
           'autocommit': True}
     try:
-        pool = yield from aiomysql.create_pool(minsize=5, maxsize=20, loop=loop, **db)
-        with (yield from pool) as conn:
-            cursor = yield from conn.cursor()
-            yield from cursor.execute(stmt_insert)
+        pool = await aiomysql.create_pool(minsize=5, maxsize=20, loop=loop, **db)
+        async with pool.acquire() as conn:
+        # with (await pool) as conn:
+            cursor = await conn.cursor()
+            await cursor.execute(stmt_insert)
         return 'Done'
     except Exception as e:
         # print('\n', sys.exc_info()[1])
@@ -28,12 +29,12 @@ def mysql_insert(loop, stmt_insert):
         # sys.exit(-1)
     finally:
         pool.close()
-        yield from pool.wait_closed()
+        await pool.wait_closed()
             # cursor.close()
             # conn.close()
 
-# the below code is fro testing
+# the below code is for testing
 stmt_insert = "insert into leotable(c) select '13'"
 loop = asyncio.get_event_loop()
-loop.run_until_complete(mysql_insert(loop,stmt_insert))
+loop.run_until_complete(mysql_insert(loop, stmt_insert))
 
